97af28bdfcfeeb962e97cf1d5b8594a1e74c3f6b
Steve Jang <cruiserx@hanmail.net>
2015-11-11 09:42:58 +0900

    license update

--- sha256_pkgbody.sql
+++ sha256_pkgbody.sql
@@ -1,9 +1,23 @@
 CREATE OR REPLACE PACKAGE BODY SHA256 IS
 /*
-   Oracle PL/SQL Package to compute SHA256 message digest of files or memory blocks.
-   according to the definition of SHA256 in FIPS 180-2.
+    Oracle PL/SQL Package to compute SHA256 message digest of files or memory blocks.
+    according to the definition of SHA256 in FIPS 180-2.
+    
+    Copyright (C) 2014, Steve Jang <cruiserx@hanmail.net>
 
-   Written by Steve Jang <cruiserx@hanmail.net>, 2014.
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
     FUNCTION BITOR (x IN NUMBER, y IN NUMBER) RETURN NUMBER AS
